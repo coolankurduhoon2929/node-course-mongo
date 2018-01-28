40a4ded48e91427dcb38c4d2ab5a1e36848474e4
Ankur Duhoon <coolankurduhoon2929@gmail.com>
2018-01-29 00:38:47 +0530

    password hash

--- server/server.js
+++ server/server.js
@@ -107,10 +107,23 @@ app.post('/users',(req,res)=>{
 });
 
 
-
 //making private route
 app.get('/users/me',authenticate,(req,res)=>{
   res.send(req.user);
+});
+
+
+//login route
+app.post('/users/login',(req,res)=>{
+  var body=_.pick(req.body,['email','password']);
+  user.findByCredentials(body.email,body.password).then((user)=>{
+    //res.send(user);
+    return user.generateAuthToken().then((token)=>{
+      res.header('x-auth',token).send(user);
+    });
+  }).catch((e)=>{
+    res.status(400).send();
+  });
 });
 
 
